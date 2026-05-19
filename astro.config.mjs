2d743fd71cd8df26e74d66e2db75814e01ea2aac
zhangcheng <897786171@qq.com>
2026-05-19 19:44:48 +0800

    Add GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://119094335.github.io',
+	base: '/czlearn',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
